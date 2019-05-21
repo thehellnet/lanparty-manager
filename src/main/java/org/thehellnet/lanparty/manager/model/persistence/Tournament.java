c7d51103d1deb810d3f40e4813eaab56f086a28f
sardylan <sardylan@gmail.com>
2019-05-22 00:37:40 +0200

    Adds Tournament model

---
+++ src/main/java/org/thehellnet/lanparty/manager/model/persistence/Tournament.java
@@ -0,0 +1,68 @@
+package org.thehellnet.lanparty.manager.model.persistence;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tournament")
+public class Tournament implements Serializable {
+
+    @Id
+    @Column(name = "id", updatable = false, nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tournament_id_seq")
+    @SequenceGenerator(name = "tournament_id_seq", sequenceName = "tournament_id_seq")
+    private Long id;
+
+    @Basic
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "game_id", nullable = false)
+    private Game game;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tournament tournament = (Tournament) o;
+        return id.equals(tournament.id) &&
+                name.equals(tournament.name) &&
+                game.equals(tournament.game);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
