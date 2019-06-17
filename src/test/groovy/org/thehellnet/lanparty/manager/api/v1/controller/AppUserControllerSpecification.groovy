2a5a55a84602d6ada9b8dcd705c1ddba5e137aea
sardylan <sardylan@gmail.com>
2019-06-17 22:47:15 +0200

    Adds test for create controller

--- src/test/groovy/org/thehellnet/lanparty/manager/api/v1/controller/AppUserControllerSpecification.groovy
+++ src/test/groovy/org/thehellnet/lanparty/manager/api/v1/controller/AppUserControllerSpecification.groovy
@@ -11,6 +11,10 @@ import org.thehellnet.utility.TokenUtility
 
 class AppUserControllerSpecification extends ControllerSpecification {
 
+    private static final String APPUSER_EMAIL = "email@email.com"
+    private static final String APPUSER_PASSWORD = "password"
+    private static final String APPUSER_NAME = "Name"
+
     def setup() {
         "Do login for token retrieving"()
     }
@@ -148,4 +152,85 @@ class AppUserControllerSpecification extends ControllerSpecification {
             assert Role.values().contains(role)
         }
     }
+
+    def "create"() {
+        given:
+        def requestBody = new JSONObject()
+        requestBody.put("email", APPUSER_EMAIL)
+        requestBody.put("password", APPUSER_PASSWORD)
+        requestBody.put("name", APPUSER_NAME)
+
+        when:
+        def rawResponse = mockMvc
+                .perform(MockMvcRequestBuilders
+                        .post("/api/v1/public/appUser/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("X-Auth-Token", token)
+                        .content(requestBody.toString())
+                )
+                .andReturn()
+                .response
+
+        then:
+        rawResponse.status == HttpStatus.OK.value()
+        MediaType.parseMediaType(rawResponse.contentType) == MediaType.APPLICATION_JSON_UTF8
+
+        JSONObject response = new JSONObject(rawResponse.contentAsString)
+
+        response.has("success")
+        response.getBoolean("success")
+
+        response.has("data")
+        JSONObject data = response.getJSONObject("data")
+
+        data.has("appUser")
+        JSONObject appUser = data.getJSONObject("appUser")
+
+        !appUser.has("password")
+
+        appUser.has("id")
+        appUser.get("id") instanceof Integer
+
+        appUser.has("email")
+        appUser.get("email") instanceof String
+        appUser.getString("email") == APPUSER_EMAIL
+
+        appUser.has("name")
+        appUser.get("name") instanceof String
+        appUser.getString("name") == APPUSER_NAME
+
+        appUser.has("appUserRoles")
+        JSONArray appUserRoles = appUser.getJSONArray("appUserRoles")
+
+        appUserRoles.length() == 0
+
+        checkAppUserNumber() == 2
+    }
+
+    private int checkAppUserNumber() {
+        JSONObject requestBody = new JSONObject()
+
+        def rawResponse = mockMvc
+                .perform(MockMvcRequestBuilders
+                        .post("/api/v1/public/appUser/getAll")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("X-Auth-Token", token)
+                        .content(requestBody.toString())
+                )
+                .andReturn()
+                .response
+
+        rawResponse.status == HttpStatus.OK.value()
+        MediaType.parseMediaType(rawResponse.contentType) == MediaType.APPLICATION_JSON_UTF8
+
+        JSONObject response = new JSONObject(rawResponse.contentAsString)
+
+        response.has("success")
+        assert response.getBoolean("success")
+
+        JSONObject data = response.getJSONObject("data")
+        JSONArray appUsers = data.getJSONArray("appUsers")
+        return appUsers.length()
+    }
+
 }
