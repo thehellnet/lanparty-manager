2ff8a8825fa3ca0dda7a664e345af5e98d395255
sardylan <sardylan@gmail.com>
2019-07-03 00:05:59 +0200

    Adds missing constructor

--- src/main/java/org/thehellnet/lanparty/manager/exception/cfg/CfgException.java
+++ src/main/java/org/thehellnet/lanparty/manager/exception/cfg/CfgException.java
@@ -2,7 +2,10 @@ package org.thehellnet.lanparty.manager.exception.cfg;
 
 import org.thehellnet.lanparty.manager.exception.LanPartyManagerException;
 
-public class CfgException extends LanPartyManagerException {
+public abstract class CfgException extends LanPartyManagerException {
+
+    public CfgException() {
+    }
 
     public CfgException(String message) {
         super(message);
