3eda399fc36755c86726f9c04f32fa781908609c
sardylan <sardylan@gmail.com>
2019-10-02 02:07:01 +0200

    REstore test and increase JAVA ram params

--- src/test/groovy/org/thehellnet/lanparty/manager/api/v1/controller/ToolControllerTest.groovy
+++ src/test/groovy/org/thehellnet/lanparty/manager/api/v1/controller/ToolControllerTest.groovy
@@ -27,187 +27,187 @@ class ToolControllerTest extends ControllerSpecification {
         createCfg()
     }
 
-//    def "ping with not existing seat"() {
-//        given:
-//        JSONObject requestBody = new JSONObject()
-//
-//        when:
-//        def rawResponse = mockMvc
-//                .perform(CustomMockMvcRequestBuilders
-//                        .post("/api/v1/tool/ping", "5.6.7.8")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(requestBody.toString()))
-//                .andReturn()
-//                .response
-//
-//        then:
-//        rawResponse.status == HttpStatus.OK.value()
-//        MediaType.parseMediaType(rawResponse.contentType) == MediaType.APPLICATION_JSON
-//
-//        when:
-//        JSONObject response = new JSONObject(rawResponse.contentAsString)
-//
-//        then:
-//        validateResponseAsJsonResponse(response)
-//
-//        response.getBoolean("success")
-//        response.get("data") == JSONObject.NULL
-//    }
-//
-//    def "ping with existing seat"() {
-//        given:
-//        JSONObject requestBody = new JSONObject()
-//
-//        when:
-//        def rawResponse = mockMvc
-//                .perform(CustomMockMvcRequestBuilders
-//                        .post("/api/v1/tool/ping", SEAT_ADDRESS)
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(requestBody.toString()))
-//                .andReturn()
-//                .response
-//
-//        then:
-//        rawResponse.status == HttpStatus.OK.value()
-//        MediaType.parseMediaType(rawResponse.contentType) == MediaType.APPLICATION_JSON
-//
-//        when:
-//        JSONObject response = new JSONObject(rawResponse.contentAsString)
-//
-//        then:
-//        validateResponseAsJsonResponse(response)
-//
-//        response.getBoolean("success")
-//        response.get("data") == JSONObject.NULL
-//    }
-//
-//    def "welcome with not existing seat"() {
-//        given:
-//        JSONObject requestBody = new JSONObject()
-//
-//        when:
-//        def rawResponse = mockMvc
-//                .perform(CustomMockMvcRequestBuilders
-//                        .post("/api/v1/tool/welcome", "5.6.7.8")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(requestBody.toString()))
-//                .andReturn()
-//                .response
-//
-//        then:
-//        rawResponse.status == HttpStatus.OK.value()
-//        MediaType.parseMediaType(rawResponse.contentType) == MediaType.APPLICATION_JSON
-//
-//        when:
-//        JSONObject response = new JSONObject(rawResponse.contentAsString)
-//
-//        then:
-//        validateResponseAsJsonResponse(response)
-//
-//        !response.getBoolean("success")
-//        response.get("data") == JSONObject.NULL
-//
-//        response.has("errors")
-//
-//        when:
-//        JSONObject errors = response.getJSONObject("errors")
-//
-//        then:
-//        errors.getString("message").length() > 0
-//    }
-//
-//    def "welcome with existing seat"() {
-//        given:
-//        JSONObject requestBody = new JSONObject()
-//
-//        when:
-//        def rawResponse = mockMvc
-//                .perform(CustomMockMvcRequestBuilders
-//                        .post("/api/v1/tool/welcome", SEAT_ADDRESS)
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(requestBody.toString()))
-//                .andReturn()
-//                .response
-//
-//        then:
-//        rawResponse.status == HttpStatus.OK.value()
-//        MediaType.parseMediaType(rawResponse.contentType) == MediaType.APPLICATION_JSON
-//
-//        when:
-//        JSONObject response = new JSONObject(rawResponse.contentAsString)
-//
-//        then:
-//        validateResponseAsJsonResponse(response)
-//
-//        response.getBoolean("success")
-//
-//        when:
-//        JSONObject data = response.getJSONObject("data")
-//
-//        then:
-//        data.has("name")
-//        data.getString("name") == SEAT_NAME
-//    }
-//
-//    def "getCfg with existing seat and existing barcode"() {
-//        given:
-//        JSONObject requestBody = new JSONObject()
-//        requestBody.put("barcode", PLAYER_BARCODE)
-//
-//        when:
-//        def rawResponse = mockMvc
-//                .perform(CustomMockMvcRequestBuilders
-//                        .post("/api/v1/tool/getCfg", SEAT_ADDRESS)
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(requestBody.toString()))
-//                .andReturn()
-//                .response
-//
-//        then:
-//        rawResponse.status == HttpStatus.OK.value()
-//        MediaType.parseMediaType(rawResponse.contentType) == MediaType.APPLICATION_JSON
-//
-//        when:
-//        JSONObject response = new JSONObject(rawResponse.contentAsString)
-//
-//        then:
-//        validateResponseAsJsonResponse(response)
-//
-//        response.getBoolean("success")
-//
-//        when:
-//        JSONArray data = response.getJSONArray("data")
-//        String cfg = StringUtility.joinLines(data.toList() as List<String>)
-//
-//        then:
-//        cfg == PLAYER_CFG_SANITIZED
-//    }
-//
-//    def "saveCfg with existing seat and existing barcode"() {
-//        given:
-//        JSONObject requestBody = new JSONObject()
-//        requestBody.put("barcode", PLAYER_BARCODE)
-//        requestBody.put("cfgLines", StringUtility.splitLines(PLAYER_CFG_SANITIZED))
-//
-//        when:
-//        def rawResponse = mockMvc
-//                .perform(CustomMockMvcRequestBuilders
-//                        .post("/api/v1/tool/saveCfg", SEAT_ADDRESS)
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(requestBody.toString()))
-//                .andReturn()
-//                .response
-//
-//        then:
-//        rawResponse.status == HttpStatus.OK.value()
-//        MediaType.parseMediaType(rawResponse.contentType) == MediaType.APPLICATION_JSON
-//
-//        when:
-//        JSONObject response = new JSONObject(rawResponse.contentAsString)
-//
-//        then:
-//        validateResponseAsJsonResponse(response)
-//
-//        response.getBoolean("success")
-//    }
+    def "ping with not existing seat"() {
+        given:
+        JSONObject requestBody = new JSONObject()
+
+        when:
+        def rawResponse = mockMvc
+                .perform(CustomMockMvcRequestBuilders
+                        .post("/api/v1/tool/ping", "5.6.7.8")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody.toString()))
+                .andReturn()
+                .response
+
+        then:
+        rawResponse.status == HttpStatus.OK.value()
+        MediaType.parseMediaType(rawResponse.contentType) == MediaType.APPLICATION_JSON
+
+        when:
+        JSONObject response = new JSONObject(rawResponse.contentAsString)
+
+        then:
+        validateResponseAsJsonResponse(response)
+
+        response.getBoolean("success")
+        response.get("data") == JSONObject.NULL
+    }
+
+    def "ping with existing seat"() {
+        given:
+        JSONObject requestBody = new JSONObject()
+
+        when:
+        def rawResponse = mockMvc
+                .perform(CustomMockMvcRequestBuilders
+                        .post("/api/v1/tool/ping", SEAT_ADDRESS)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody.toString()))
+                .andReturn()
+                .response
+
+        then:
+        rawResponse.status == HttpStatus.OK.value()
+        MediaType.parseMediaType(rawResponse.contentType) == MediaType.APPLICATION_JSON
+
+        when:
+        JSONObject response = new JSONObject(rawResponse.contentAsString)
+
+        then:
+        validateResponseAsJsonResponse(response)
+
+        response.getBoolean("success")
+        response.get("data") == JSONObject.NULL
+    }
+
+    def "welcome with not existing seat"() {
+        given:
+        JSONObject requestBody = new JSONObject()
+
+        when:
+        def rawResponse = mockMvc
+                .perform(CustomMockMvcRequestBuilders
+                        .post("/api/v1/tool/welcome", "5.6.7.8")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody.toString()))
+                .andReturn()
+                .response
+
+        then:
+        rawResponse.status == HttpStatus.OK.value()
+        MediaType.parseMediaType(rawResponse.contentType) == MediaType.APPLICATION_JSON
+
+        when:
+        JSONObject response = new JSONObject(rawResponse.contentAsString)
+
+        then:
+        validateResponseAsJsonResponse(response)
+
+        !response.getBoolean("success")
+        response.get("data") == JSONObject.NULL
+
+        response.has("errors")
+
+        when:
+        JSONObject errors = response.getJSONObject("errors")
+
+        then:
+        errors.getString("message").length() > 0
+    }
+
+    def "welcome with existing seat"() {
+        given:
+        JSONObject requestBody = new JSONObject()
+
+        when:
+        def rawResponse = mockMvc
+                .perform(CustomMockMvcRequestBuilders
+                        .post("/api/v1/tool/welcome", SEAT_ADDRESS)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody.toString()))
+                .andReturn()
+                .response
+
+        then:
+        rawResponse.status == HttpStatus.OK.value()
+        MediaType.parseMediaType(rawResponse.contentType) == MediaType.APPLICATION_JSON
+
+        when:
+        JSONObject response = new JSONObject(rawResponse.contentAsString)
+
+        then:
+        validateResponseAsJsonResponse(response)
+
+        response.getBoolean("success")
+
+        when:
+        JSONObject data = response.getJSONObject("data")
+
+        then:
+        data.has("name")
+        data.getString("name") == SEAT_NAME
+    }
+
+    def "getCfg with existing seat and existing barcode"() {
+        given:
+        JSONObject requestBody = new JSONObject()
+        requestBody.put("barcode", PLAYER_BARCODE)
+
+        when:
+        def rawResponse = mockMvc
+                .perform(CustomMockMvcRequestBuilders
+                        .post("/api/v1/tool/getCfg", SEAT_ADDRESS)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody.toString()))
+                .andReturn()
+                .response
+
+        then:
+        rawResponse.status == HttpStatus.OK.value()
+        MediaType.parseMediaType(rawResponse.contentType) == MediaType.APPLICATION_JSON
+
+        when:
+        JSONObject response = new JSONObject(rawResponse.contentAsString)
+
+        then:
+        validateResponseAsJsonResponse(response)
+
+        response.getBoolean("success")
+
+        when:
+        JSONArray data = response.getJSONArray("data")
+        String cfg = StringUtility.joinLines(data.toList() as List<String>)
+
+        then:
+        cfg == PLAYER_CFG_SANITIZED
+    }
+
+    def "saveCfg with existing seat and existing barcode"() {
+        given:
+        JSONObject requestBody = new JSONObject()
+        requestBody.put("barcode", PLAYER_BARCODE)
+        requestBody.put("cfgLines", StringUtility.splitLines(PLAYER_CFG_SANITIZED))
+
+        when:
+        def rawResponse = mockMvc
+                .perform(CustomMockMvcRequestBuilders
+                        .post("/api/v1/tool/saveCfg", SEAT_ADDRESS)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody.toString()))
+                .andReturn()
+                .response
+
+        then:
+        rawResponse.status == HttpStatus.OK.value()
+        MediaType.parseMediaType(rawResponse.contentType) == MediaType.APPLICATION_JSON
+
+        when:
+        JSONObject response = new JSONObject(rawResponse.contentAsString)
+
+        then:
+        validateResponseAsJsonResponse(response)
+
+        response.getBoolean("success")
+    }
 }
