30d6b74ebdeaddec498ae1e0cc6ac623639e2d99
sardylan <sardylan@gmail.com>
2019-10-11 00:00:48 +0200

    Adds delete tests

--- src/test/groovy/org/thehellnet/lanparty/manager/api/v1/controller/AppUserControllerSpecification.groovy
+++ src/test/groovy/org/thehellnet/lanparty/manager/api/v1/controller/AppUserControllerSpecification.groovy
@@ -565,360 +565,64 @@ class AppUserControllerSpecification extends ControllerSpecification {
         appUser.getString("barcode") == APPUSER_BARCODE_NEW
     }
 
-//    MockHttpServletResponse doPost(String url, JSONObject requestBody = new JSONObject()) {
-//        MockHttpServletResponse httpServletResponse = mockMvc
-//                .perform(MockMvcRequestBuilders
-//                        .post(url)
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .header("X-Auth-Token", token)
-//                        .content(requestBody.toString())
-//                )
-//                .andReturn()
-//                .response
-//
-//        assert MediaType.parseMediaType(httpServletResponse.contentType) == MediaType.APPLICATION_JSON
-//
-//        return httpServletResponse
-//    }
-//
-//    def "get"() {
-//        given:
-//        def requestBody = new JSONObject()
-//        requestBody.put("id", 1)
-//
-//        when:
-//        def rawResponse = mockMvc
-//                .perform(MockMvcRequestBuilders
-//                        .post("/api/v1/public/appUser/get")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .header("X-Auth-Token", token)
-//                        .content(requestBody.toString())
-//                )
-//                .andReturn()
-//                .response
-//
-//        then:
-//        rawResponse.status == HttpStatus.OK.value()
-//        MediaType.parseMediaType(rawResponse.contentType) == MediaType.APPLICATION_JSON
-//
-//        when:
-//        JSONObject response = new JSONObject(rawResponse.contentAsString)
-//
-//        then:
-//        validateResponseAsJsonResponse(response)
-//
-//        response.getBoolean("success")
-//
-//        when:
-//        JSONObject appUser = response.getJSONObject("data")
-//
-//        then:
-//        !appUser.has("password")
-//
-//        appUser.has("id")
-//        appUser.get("id") instanceof Integer
-//
-//        appUser.has("email")
-//        appUser.get("email") instanceof String
-//
-//        appUser.has("name")
-//        appUser.get("name") == JSONObject.NULL
-//
-//        appUser.has("appUserRoles")
-//
-//        when:
-//        JSONArray appUserRoles = appUser.getJSONArray("appUserRoles")
-//
-//        then:
-//        appUserRoles.length() == Role.values().length
-//
-//        for (int i = 0; i < appUserRoles.length(); i++) {
-//            Role role = Role.valueOf(appUserRoles.getString(i))
-//            assert Role.values().contains(role)
-//        }
-//    }
-//
-//    def "save"() {
-//        setup:
-//        Long appUserId = 'retrieve test appUser ID and creates if not exists'()
-//
-//        expect:
-//        appUserId != null
-//
-//        when:
-//        JSONObject requestBody = new JSONObject()
-//        requestBody.put("id", appUserId)
-//        requestBody.put("name", APPUSER_NAME_NEW)
-//        requestBody.put("appUserRoles", new JSONArray(APPUSER_ROLES_NEW))
-//
-//        def rawResponse = mockMvc
-//                .perform(MockMvcRequestBuilders
-//                        .post("/api/v1/public/appUser/save")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .header("X-Auth-Token", token)
-//                        .content(requestBody.toString())
-//                )
-//                .andReturn()
-//                .response
-//
-//        then:
-//        rawResponse.status == HttpStatus.OK.value()
-//        MediaType.parseMediaType(rawResponse.contentType) == MediaType.APPLICATION_JSON
-//
-//        when:
-//        JSONObject response = new JSONObject(rawResponse.contentAsString)
-//
-//        then:
-//        validateResponseAsJsonResponse(response)
-//
-//        response.getBoolean("success")
-//
-//        when:
-//        JSONObject appUser = response.getJSONObject("data")
-//
-//        then:
-//        !appUser.has("password")
-//
-//        appUser.has("id")
-//        appUser.get("id") instanceof Integer
-//        appUser.getLong("id") == appUserId
-//
-//        appUser.has("email")
-//        appUser.get("email") instanceof String
-//        appUser.getString("email") == APPUSER_EMAIL
-//
-//        appUser.has("name")
-//        appUser.get("name") instanceof String
-//        appUser.getString("name") == APPUSER_NAME_NEW
-//
-//        appUser.has("appUserRoles")
-//
-//        when:
-//        JSONArray appUserRoles = appUser.getJSONArray("appUserRoles")
-//
-//        then:
-//        appUserRoles.length() == APPUSER_ROLES_NEW.length
-//
-//        "check number of appUsers in database"() == 2
-//    }
-//
-//    def "delete"() {
-//        setup:
-//        Long appUserId = 'retrieve test appUser ID and creates if not exists'()
-//
-//        expect:
-//        appUserId != null
-//
-//        when:
-//        JSONObject requestBody = new JSONObject()
-//        requestBody.put("id", appUserId)
-//
-//        def rawResponse = mockMvc
-//                .perform(MockMvcRequestBuilders
-//                        .post("/api/v1/public/appUser/delete")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .header("X-Auth-Token", token)
-//                        .content(requestBody.toString())
-//                )
-//                .andReturn()
-//                .response
-//
-//        then:
-//        rawResponse.status == HttpStatus.OK.value()
-//        MediaType.parseMediaType(rawResponse.contentType) == MediaType.APPLICATION_JSON
-//
-//        when:
-//        JSONObject response = new JSONObject(rawResponse.contentAsString)
-//
-//        then:
-//        validateResponseAsJsonResponse(response)
-//
-//        response.getBoolean("success")
-//
-//        "check number of appUsers in database"() == 1
-//    }
-//
-//    def "getAll"() {
-//        when:
-//        def rawResponse = doPost("/api/v1/public/appUser/getAll")
-//
-//        then:
-//        rawResponse.status == HttpStatus.OK.value()
-//
-//        when:
-//        JSONObject response = new JSONObject(rawResponse.contentAsString)
-//
-//        then:
-//        validateResponseAsJsonResponse(response)
-//
-//        response.getBoolean("success")
-//
-//        when:
-//        JSONArray appUsers = response.getJSONArray("data")
-//
-//        then:
-//        appUsers.length() == 1
-//
-//        when:
-//        JSONObject appUser = appUsers.getJSONObject(0)
-//
-//        then:
-//        appUser.has("id")
-//        appUser.get("id") instanceof Integer
-//
-//        appUser.has("email")
-//        appUser.get("email") instanceof String
-//
-//        appUser.has("name")
-//        appUser.get("name") == JSONObject.NULL
-//    }
-//
-//    def "login"() {
-//        given:
-//        JSONObject requestBody = new JSONObject()
-//        requestBody.put("email", "admin")
-//        requestBody.put("password", "admin")
-//
-//        when:
-//        def rawResponse = mockMvc
-//                .perform(MockMvcRequestBuilders
-//                        .post("/api/v1/public/appUser/login")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(requestBody.toString())
-//                )
-//                .andReturn()
-//                .response
-//
-//        then:
-//        rawResponse.status == HttpStatus.OK.value()
-//        MediaType.parseMediaType(rawResponse.contentType) == MediaType.APPLICATION_JSON
-//
-//        when:
-//        JSONObject response = new JSONObject(rawResponse.contentAsString)
-//
-//        then:
-//        validateResponseAsJsonResponse(response)
-//
-//        response.getBoolean("success")
-//
-//        when:
-//        JSONObject data = response.getJSONObject("data")
-//
-//        then:
-//        data.has("expiration")
-//        new DateTime(data.getLong("expiration")).isAfterNow()
-//
-//        data.has("token")
-//        data.getString("token").length() == TokenUtility.LENGTH
-//    }
-//
-//    def "changePassword"() {
-//        setup:
-//        Long appUserId = "retrieve test appUser ID and creates if not exists"()
-//        String appUserToken = "get appUser token via login"()
-//
-//        expect:
-//        appUserId != null
-//        appUserToken != null
-//
-//        when: "Token of appUser 'admin'"
-//        JSONObject requestBody = new JSONObject()
-//        requestBody.put("oldPassword", APPUSER_PASSWORD)
-//        requestBody.put("newPassword", APPUSER_PASSWORD_NEW)
-//
-//        def rawResponse = mockMvc
-//                .perform(MockMvcRequestBuilders
-//                        .post("/api/v1/public/appUser/changePassword")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .header("X-Auth-Token", token)
-//                        .content(requestBody.toString())
-//                )
-//                .andReturn()
-//                .response
-//
-//        then:
-//        rawResponse.status == HttpStatus.OK.value()
-//        MediaType.parseMediaType(rawResponse.contentType) == MediaType.APPLICATION_JSON
-//
-//        when:
-//        JSONObject response = new JSONObject(rawResponse.contentAsString)
-//
-//        then:
-//        validateResponseAsJsonResponse(response)
-//
-//        !response.getBoolean("success")
-//
-//        when:
-//        "Token of appUser '${APPUSER_NAME}'"
-//        requestBody = new JSONObject()
-//        requestBody.put("oldPassword", APPUSER_PASSWORD)
-//        requestBody.put("newPassword", APPUSER_PASSWORD_NEW)
-//
-//        rawResponse = mockMvc
-//                .perform(MockMvcRequestBuilders
-//                        .post("/api/v1/public/appUser/changePassword")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .header("X-Auth-Token", appUserToken)
-//                        .content(requestBody.toString())
-//                )
-//                .andReturn()
-//                .response
-//
-//        then:
-//        rawResponse.status == HttpStatus.OK.value()
-//        MediaType.parseMediaType(rawResponse.contentType) == MediaType.APPLICATION_JSON
-//
-//        when:
-//        response = new JSONObject(rawResponse.contentAsString)
-//
-//        then:
-//        validateResponseAsJsonResponse(response)
-//
-//        response.getBoolean("success")
-//    }
-//
-//    private Long 'retrieve test appUser ID and creates if not exists'() {
-//        AppUser appUser = appUserService.findByEmail(APPUSER_EMAIL)
-//
-//        if (appUser != null) {
-//            appUserService.delete(appUser.id)
-//            appUser = null
-//        }
-//
-//        if (appUser == null) {
-//            appUser = appUserService.create(APPUSER_EMAIL, APPUSER_PASSWORD, APPUSER_NAME)
-//        }
-//
-//        appUser = appUserService.update(appUser.id, null, APPUSER_ROLES_NEW)
-//
-//        return appUser.id
-//    }
-//
-//    private String "get appUser token via login"() {
-//        JSONObject requestBody = new JSONObject()
-//        requestBody.put("email", APPUSER_EMAIL)
-//        requestBody.put("password", APPUSER_PASSWORD)
-//
-//        def rawResponse = mockMvc
-//                .perform(MockMvcRequestBuilders
-//                        .post("/api/v1/public/appUser/login")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(requestBody.toString())
-//                )
-//                .andReturn()
-//                .response
-//
-//        rawResponse.status == HttpStatus.OK.value()
-//        JSONObject response = new JSONObject(rawResponse.contentAsString)
-//
-//        if (!response.getBoolean("success")) {
-//            return null
-//        }
-//
-//        JSONObject data = response.getJSONObject("data")
-//
-//        return data.getString("token")
-//    }
+    def "delete without id"() {
+        when:
+        def rawResponse = mockMvc
+                .perform(MockMvcRequestBuilders
+                        .delete("/api/v1/public/appUser")
+                        .header("X-Auth-Token", token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andReturn()
+                .response
+
+        then:
+        rawResponse.status == HttpStatus.METHOD_NOT_ALLOWED.value()
+    }
+
+    def "delete with wrong appUserId"() {
+        given:
+        Long appUserId = 123
+        JSONObject requestBody = new JSONObject()
+
+        when:
+        def rawResponse = mockMvc
+                .perform(MockMvcRequestBuilders
+                        .delete("/api/v1/public/appUser/${appUserId}")
+                        .header("X-Auth-Token", token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody.toString())
+                )
+                .andReturn()
+                .response
+
+        then:
+        rawResponse.status == HttpStatus.NOT_FOUND.value()
+    }
+
+    def "delete with valid appUserId"() {
+        given:
+        Long appUserId = appUserRepository.save(new AppUser(APPUSER_NAME, APPUSER_PASSWORD)).id
+        JSONObject requestBody = new JSONObject()
+
+        when:
+        def rawResponse = mockMvc
+                .perform(MockMvcRequestBuilders
+                        .delete("/api/v1/public/appUser/${appUserId}")
+                        .header("X-Auth-Token", token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody.toString())
+                )
+                .andReturn()
+                .response
+
+        then:
+        rawResponse.status == HttpStatus.NO_CONTENT.value()
+
+        and:
+        "check number of appUsers in database"() == 1
+    }
+
 
     def "isTokenValid without token"() {
         when:
