05515ca740091dc6f7daca95b607096ff76b1baf
sardylan <sardylan@gmail.com>
2019-10-17 01:14:21 +0200

    Adds Match controller

--- src/main/java/org/thehellnet/lanparty/manager/api/v1/controller/MatchController.java
+++ src/main/java/org/thehellnet/lanparty/manager/api/v1/controller/MatchController.java
@@ -1,4 +1,94 @@
 package org.thehellnet.lanparty.manager.api.v1.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.thehellnet.lanparty.manager.api.v1.controller.aspect.CheckRoles;
+import org.thehellnet.lanparty.manager.api.v1.controller.aspect.CheckToken;
+import org.thehellnet.lanparty.manager.model.constant.Role;
+import org.thehellnet.lanparty.manager.model.dto.request.match.CreateMatchRequestDTO;
+import org.thehellnet.lanparty.manager.model.dto.request.match.UpdateMatchRequestDTO;
+import org.thehellnet.lanparty.manager.model.persistence.AppUser;
+import org.thehellnet.lanparty.manager.model.persistence.Match;
+import org.thehellnet.lanparty.manager.service.MatchService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping(path = "/api/v1/public/match")
 public class MatchController {
+
+    private static final Logger logger = LoggerFactory.getLogger(MatchController.class);
+
+    private final MatchService matchService;
+
+    @Autowired
+    public MatchController(MatchService matchService) {
+        this.matchService = matchService;
+    }
+
+    @CheckToken
+    @CheckRoles(Role.APPUSER_ADMIN)
+    @RequestMapping(
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity create(HttpServletRequest request, AppUser appUser, @RequestBody CreateMatchRequestDTO dto) {
+        Match match = matchService.create(dto.name, dto.tournament, dto.server, dto.gameMap, dto.gametype, dto.localTeam, dto.guestTeam);
+        return ResponseEntity.created(URI.create("")).body(match);
+    }
+
+    @CheckToken
+    @CheckRoles(Role.APPUSER_VIEW)
+    @RequestMapping(
+            path = "{id}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity read(HttpServletRequest request, AppUser appUser, @PathVariable(value = "id") Long id) {
+        Match match = matchService.get(id);
+        return ResponseEntity.ok(match);
+    }
+
+    @CheckToken
+    @CheckRoles(Role.APPUSER_VIEW)
+    @RequestMapping(
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity read(HttpServletRequest request, AppUser appUser) {
+        List<Match> matchs = matchService.getAll();
+        return ResponseEntity.ok(matchs);
+    }
+
+    @CheckToken
+    @CheckRoles(Role.APPUSER_ADMIN)
+    @RequestMapping(
+            path = "{id}",
+            method = RequestMethod.PATCH,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity update(HttpServletRequest request, AppUser appUser, @PathVariable(value = "id") Long id, @RequestBody UpdateMatchRequestDTO dto) {
+        Match match = matchService.update(id, dto.name, dto.tournament, dto.server, dto.gameMap, dto.gametype, dto.localTeam, dto.guestTeam);
+        return ResponseEntity.ok(match);
+    }
+
+    @CheckToken
+    @CheckRoles(Role.APPUSER_ADMIN)
+    @RequestMapping(
+            path = "{id}",
+            method = RequestMethod.DELETE
+    )
+    public ResponseEntity delete(HttpServletRequest request, AppUser appUser, @PathVariable(value = "id") Long id) {
+        matchService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/org/thehellnet/lanparty/manager/service/MatchService.java
+++ src/main/java/org/thehellnet/lanparty/manager/service/MatchService.java
@@ -9,7 +9,7 @@ import org.thehellnet.lanparty.manager.exception.controller.InvalidDataException
 import org.thehellnet.lanparty.manager.exception.controller.NotFoundException;
 import org.thehellnet.lanparty.manager.exception.controller.UnchangedException;
 import org.thehellnet.lanparty.manager.model.persistence.*;
-import org.thehellnet.lanparty.manager.repository.MatchRepository;
+import org.thehellnet.lanparty.manager.repository.*;
 
 import java.util.List;
 
@@ -19,23 +19,42 @@ public class MatchService extends AbstractService {
     private static final Logger logger = LoggerFactory.getLogger(TeamService.class);
 
     private final MatchRepository matchRepository;
+    private final TournamentRepository tournamentRepository;
+    private final ServerRepository serverRepository;
+    private final GameMapRepository gameMapRepository;
+    private final GametypeRepository gametypeRepository;
+    private final TeamRepository teamRepository;
 
     @Autowired
-    public MatchService(MatchRepository matchRepository) {
+    public MatchService(MatchRepository matchRepository, TournamentRepository tournamentRepository, ServerRepository serverRepository, GameMapRepository gameMapRepository, GametypeRepository gametypeRepository, TeamRepository teamRepository) {
         this.matchRepository = matchRepository;
+        this.tournamentRepository = tournamentRepository;
+        this.serverRepository = serverRepository;
+        this.gameMapRepository = gameMapRepository;
+        this.gametypeRepository = gametypeRepository;
+        this.teamRepository = teamRepository;
     }
 
     @Transactional
-    public Match create(String name, Tournament tournament, Server server, GameMap gameMap, Gametype gametype, Team localTeam, Team guestTeam) {
+    public Match create(String name, Long tournamentId, Long serverId, Long gameMapId, Long gametypeId, Long localTeamId, Long guestTeamId) {
         if (name == null) {
-            throw new InvalidDataException("Invalid tournament");
+            throw new InvalidDataException("Invalid name");
         }
+
+        Tournament tournament = tournamentRepository.findById(tournamentId).orElse(null);
         if (tournament == null) {
             throw new InvalidDataException("Invalid tournament");
         }
+
+        Server server = serverRepository.findById(serverId).orElse(null);
         if (server == null) {
-            throw new InvalidDataException("Invalid tournament");
+            throw new InvalidDataException("Invalid server");
         }
+
+        GameMap gameMap = gameMapRepository.findById(gameMapId).orElse(null);
+        Gametype gametype = gametypeRepository.findById(gametypeId).orElse(null);
+        Team localTeam = teamRepository.findById(localTeamId).orElse(null);
+        Team guestTeam = teamRepository.findById(guestTeamId).orElse(null);
 
         Match match = new Match(name, tournament, server, gameMap, gametype, localTeam, guestTeam);
         match = matchRepository.save(match);
@@ -53,7 +72,7 @@ public class MatchService extends AbstractService {
     }
 
     @Transactional
-    public Match update(Long id, String name, Tournament tournament, Server server, GameMap gameMap, Gametype gametype, Team localTeam, Team guestTeam) {
+    public Match update(Long id, String name, Long tournamentId, Long serverId, Long gameMapId, Long gametypeId, Long localTeamId, Long guestTeamId) {
         Match match = findById(id);
 
         boolean changed = false;
@@ -63,33 +82,69 @@ public class MatchService extends AbstractService {
             changed = true;
         }
 
-        if (tournament != null) {
+        if (tournamentId != null) {
+            Tournament tournament = tournamentRepository.findById(tournamentId).orElse(null);
+            if (tournament == null) {
+                throw new InvalidDataException("Invalid tournament");
+            }
             match.setTournament(tournament);
             changed = true;
         }
 
-        if (server != null) {
+        if (serverId != null) {
+            Server server = serverRepository.findById(serverId).orElse(null);
+            if (server == null) {
+                throw new InvalidDataException("Invalid server");
+            }
             match.setServer(server);
             changed = true;
         }
 
-        if (gameMap != null) {
+        if (gameMapId != null) {
+            GameMap gameMap = gameMapRepository.findById(gameMapId).orElse(null);
+            if (gameMap == null) {
+                throw new InvalidDataException("Invalid gameMap");
+            }
             match.setGameMap(gameMap);
             changed = true;
+        } else if (match.getGameMap() != null) {
+            match.setGameMap(null);
+            changed = true;
         }
 
-        if (gametype != null) {
+        if (gametypeId != null) {
+            Gametype gametype = gametypeRepository.findById(gametypeId).orElse(null);
+            if (gametype == null) {
+                throw new InvalidDataException("Invalid gametype");
+            }
             match.setGametype(gametype);
             changed = true;
-        }
-
-        if (localTeam != null) {
-            match.setLocalTeam(localTeam);
+        } else if (match.getGametype() != null) {
+            match.setGametype(null);
             changed = true;
         }
 
-        if (guestTeam != null) {
+        if (localTeamId != null) {
+            Team localTeam = teamRepository.findById(localTeamId).orElse(null);
+            if (localTeam == null) {
+                throw new InvalidDataException("Invalid localTeam");
+            }
+            match.setLocalTeam(localTeam);
+            changed = true;
+        } else if (match.getLocalTeam() != null) {
+            match.setLocalTeam(null);
+            changed = true;
+        }
+
+        if (guestTeamId != null) {
+            Team guestTeam = teamRepository.findById(guestTeamId).orElse(null);
+            if (guestTeam == null) {
+                throw new InvalidDataException("Invalid guestTeam");
+            }
             match.setGuestTeam(guestTeam);
+            changed = true;
+        } else if (match.getGuestTeam() != null) {
+            match.setGuestTeam(null);
             changed = true;
         }
 
