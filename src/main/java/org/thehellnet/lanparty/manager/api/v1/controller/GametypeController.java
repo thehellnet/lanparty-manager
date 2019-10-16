e68579d38ae5b8e040fbf15c71d7c92936887ea8
sardylan <sardylan@gmail.com>
2019-10-17 01:04:17 +0200

    Adds Gametype controller

--- src/main/java/org/thehellnet/lanparty/manager/api/v1/controller/GametypeController.java
+++ src/main/java/org/thehellnet/lanparty/manager/api/v1/controller/GametypeController.java
@@ -1,22 +1,94 @@
 package org.thehellnet.lanparty.manager.api.v1.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.thehellnet.lanparty.manager.repository.GameRepository;
-import org.thehellnet.lanparty.manager.repository.GametypeRepository;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.thehellnet.lanparty.manager.api.v1.controller.aspect.CheckRoles;
+import org.thehellnet.lanparty.manager.api.v1.controller.aspect.CheckToken;
+import org.thehellnet.lanparty.manager.model.constant.Role;
+import org.thehellnet.lanparty.manager.model.dto.request.gametype.CreateGametypeRequestDTO;
+import org.thehellnet.lanparty.manager.model.dto.request.gametype.UpdateGametypeRequestDTO;
+import org.thehellnet.lanparty.manager.model.persistence.AppUser;
+import org.thehellnet.lanparty.manager.model.persistence.Gametype;
+import org.thehellnet.lanparty.manager.service.GametypeService;
 
-@Controller
+import javax.servlet.http.HttpServletRequest;
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(path = "/api/v1/public/gametype")
 public class GametypeController {
 
-    private final GameRepository gameRepository;
-    private final GametypeRepository gametypeRepository;
+    private static final Logger logger = LoggerFactory.getLogger(GametypeController.class);
+
+    private final GametypeService gametypeService;
 
     @Autowired
-    public GametypeController(GameRepository gameRepository, GametypeRepository gametypeRepository) {
-        this.gameRepository = gameRepository;
-        this.gametypeRepository = gametypeRepository;
+    public GametypeController(GametypeService gametypeService) {
+        this.gametypeService = gametypeService;
     }
 
+    @CheckToken
+    @CheckRoles(Role.APPUSER_ADMIN)
+    @RequestMapping(
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity create(HttpServletRequest request, AppUser appUser, @RequestBody CreateGametypeRequestDTO dto) {
+        Gametype gametype = gametypeService.create(dto.name);
+        return ResponseEntity.created(URI.create("")).body(gametype);
+    }
+
+    @CheckToken
+    @CheckRoles(Role.APPUSER_VIEW)
+    @RequestMapping(
+            path = "{id}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity read(HttpServletRequest request, AppUser appUser, @PathVariable(value = "id") Long id) {
+        Gametype gametype = gametypeService.get(id);
+        return ResponseEntity.ok(gametype);
+    }
+
+    @CheckToken
+    @CheckRoles(Role.APPUSER_VIEW)
+    @RequestMapping(
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity read(HttpServletRequest request, AppUser appUser) {
+        List<Gametype> gametypes = gametypeService.getAll();
+        return ResponseEntity.ok(gametypes);
+    }
+
+    @CheckToken
+    @CheckRoles(Role.APPUSER_ADMIN)
+    @RequestMapping(
+            path = "{id}",
+            method = RequestMethod.PATCH,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity update(HttpServletRequest request, AppUser appUser, @PathVariable(value = "id") Long id, @RequestBody UpdateGametypeRequestDTO dto) {
+        Gametype gametype = gametypeService.update(id, dto.name);
+        return ResponseEntity.ok(gametype);
+    }
+
+    @CheckToken
+    @CheckRoles(Role.APPUSER_ADMIN)
+    @RequestMapping(
+            path = "{id}",
+            method = RequestMethod.DELETE
+    )
+    public ResponseEntity delete(HttpServletRequest request, AppUser appUser, @PathVariable(value = "id") Long id) {
+        gametypeService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/org/thehellnet/lanparty/manager/model/dto/request/gametype/UpdateGametypeRequestDTO.java
+++ src/main/java/org/thehellnet/lanparty/manager/model/dto/request/gametype/UpdateGametypeRequestDTO.java
@@ -1,4 +1,6 @@
 package org.thehellnet.lanparty.manager.model.dto.request.gametype;
 
 public class UpdateGametypeRequestDTO extends GametypeRequestDTO {
+
+    public String name;
 }
