81ac4b4865c7354479af2d2f78fec6b3e0c3db61
sardylan <sardylan@gmail.com>
2019-11-15 13:54:31 +0100

    Adds swagger

---
+++ src/main/java/org/thehellnet/lanparty/manager/configuration/MvcConfiguration.java
@@ -0,0 +1,21 @@
+package org.thehellnet.lanparty.manager.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
