58bfea4e61901ddb2c532e04a8526c93db0f39cd
sardylan <sardylan@gmail.com>
2019-12-04 00:37:15 +0100

    Websocket works

--- src/main/java/org/thehellnet/lanparty/manager/configuration/SpringConfiguration.java
+++ src/main/java/org/thehellnet/lanparty/manager/configuration/SpringConfiguration.java
@@ -1,8 +1,12 @@
 package org.thehellnet.lanparty.manager.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -17,6 +21,7 @@ import javax.servlet.ServletRegistration;
 @ComponentScan(basePackages = "org.thehellnet.lanparty.manager")
 @EnableWebMvc
 @EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableScheduling
 public class SpringConfiguration implements WebApplicationInitializer {
 
     @Override
@@ -32,5 +37,16 @@ public class SpringConfiguration implements WebApplicationInitializer {
         ServletRegistration.Dynamic servletRegistration = servletContext.addServlet("servlet", dispatcherServlet);
         servletRegistration.setLoadOnStartup(1);
         servletRegistration.addMapping("/*");
+    }
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+
+        scheduler.setPoolSize(2);
+        scheduler.setThreadNamePrefix("scheduled-task-");
+        scheduler.setDaemon(true);
+
+        return scheduler;
     }
 }
--- src/main/java/org/thehellnet/lanparty/manager/service/ShowcaseService.java
+++ src/main/java/org/thehellnet/lanparty/manager/service/ShowcaseService.java
@@ -2,13 +2,16 @@ package org.thehellnet.lanparty.manager.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.thehellnet.lanparty.manager.exception.controller.NotFoundException;
 import org.thehellnet.lanparty.manager.model.persistence.Showcase;
 import org.thehellnet.lanparty.manager.repository.ShowcaseRepository;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -17,6 +20,8 @@ import java.util.Objects;
 public class ShowcaseService extends AbstractService {
 
     private static final Logger logger = LoggerFactory.getLogger(ShowcaseService.class);
+
+    private final Object SYNC = new Object();
 
     private final ShowcaseRepository showcaseRepository;
 
@@ -37,31 +42,52 @@ public class ShowcaseService extends AbstractService {
 
     @Transactional
     public void addShowcase(String tag, WebSocketSession webSocketSession) {
-        String ipAddress = Objects.requireNonNull(webSocketSession.getRemoteAddress()).getAddress().getHostAddress();
+        synchronized (SYNC) {
+            String ipAddress = Objects.requireNonNull(webSocketSession.getRemoteAddress()).getAddress().getHostAddress();
 
-        Showcase showcase = showcaseRepository.findByTag(tag);
-        if (showcase == null) {
-            showcase = new Showcase(tag);
+            Showcase showcase = showcaseRepository.findByTag(tag);
+            if (showcase == null) {
+                showcase = new Showcase(tag);
+            }
+
+            showcase.setConnected(true);
+            showcase.setLastAddress(ipAddress);
+            showcase.updateLastContact();
+            showcase = showcaseRepository.save(showcase);
+
+            sessions.put(showcase, webSocketSession);
+
+            logger.info("Showcase {} added from {}", tag, ipAddress);
         }
-
-        showcase.setConnected(true);
-        showcase.setLastAddress(ipAddress);
-        showcase.updateLastContact();
-        showcase = showcaseRepository.save(showcase);
-
-        sessions.put(showcase, webSocketSession);
-
-        logger.info("Showcase {} added from {}", tag, ipAddress);
     }
 
     @Transactional
     public void removeShowcase(String tag) {
-        Showcase showcase = findByTag(tag);
-        showcase.setConnected(false);
-        showcase = showcaseRepository.save(showcase);
+        synchronized (SYNC) {
+            Showcase showcase = findByTag(tag);
+            showcase.setConnected(false);
+            showcase = showcaseRepository.save(showcase);
 
-        sessions.remove(showcase);
+            sessions.remove(showcase);
 
-        logger.info("Showcase {} removed", tag);
+            logger.info("Showcase {} removed", tag);
+        }
+    }
+
+    @Scheduled(fixedDelay = 2000)
+    public void sendToAllShowcases() {
+        synchronized (SYNC) {
+            sessions.forEach((showcase, webSocketSession) -> {
+                logger.info("Sendig to {}", showcase);
+
+                TextMessage message = new TextMessage("Hi");
+
+                try {
+                    webSocketSession.sendMessage(message);
+                } catch (IOException e) {
+                    logger.error(e.getMessage());
+                }
+            });
+        }
     }
 }
