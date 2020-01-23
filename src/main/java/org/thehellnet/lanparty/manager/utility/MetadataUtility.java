458e884e42e6d56666f4560815dca15764487aeb
sardylan <sardylan@gmail.com>
2020-01-23 22:21:08 +0100

    Fixes metadata

--- src/main/java/org/thehellnet/lanparty/manager/utility/MetadataUtility.java
+++ src/main/java/org/thehellnet/lanparty/manager/utility/MetadataUtility.java
@@ -3,16 +3,19 @@ package org.thehellnet.lanparty.manager.utility;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.thehellnet.utility.StringUtility;
 
 import javax.persistence.*;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class MetadataUtility {
+
+    private static final Logger logger = LoggerFactory.getLogger(MetadataUtility.class);
 
     private static final Class<?>[] FIELD_TYPES = new Class[]{
             Id.class,
@@ -36,54 +39,90 @@ public class MetadataUtility {
 
     public JSONArray compute() {
         Reflections reflections = new Reflections("org.thehellnet.lanparty.manager.model.persistence");
-        Set<Class<?>> entityClasses = reflections.getTypesAnnotatedWith(Entity.class);
+        Set<Class<?>> entityClassSet = reflections.getTypesAnnotatedWith(Entity.class);
+
+        List<Class<?>> entityClasses = new ArrayList<>(entityClassSet);
+        entityClasses.sort(Comparator.comparing(Class::getSimpleName));
 
         JSONArray metadata = new JSONArray();
 
         for (Class<?> entityClass : entityClasses) {
-            JSONArray fields = new JSONArray();
-
-            for (Field field : entityClass.getDeclaredFields()) {
-                String type = null;
-
-                Annotation[] annotations = field.getAnnotations();
-
-                for (Class<?> clazz : FIELD_TYPES) {
-                    for (Annotation annotation : annotations) {
-                        if (annotation.annotationType().getSimpleName().equals(clazz.getSimpleName())) {
-                            type = clazz.getSimpleName();
-                            break;
-                        }
-
-                        if (type != null) {
-                            break;
-                        }
-                    }
-                }
-
-                String className = field.getType().getSimpleName();
-
-                for (Class<?> clazz : GENERICS_TYPES) {
-                    if (className.equals(clazz.getSimpleName())) {
-                        String[] items = ((ParameterizedType) field.getGenericType()).getActualTypeArguments()[0].getTypeName().split("\\.");
-                        className = items[items.length - 1];
-                        break;
-                    }
-                }
-
-                JSONObject fieldObj = new JSONObject();
-                fieldObj.put("name", field.getName());
-                fieldObj.put("type", type);
-                fieldObj.put("class", StringUtility.firstLetterLowercase(className));
-                fields.put(fieldObj);
-            }
-
-            JSONObject entityObj = new JSONObject();
-            entityObj.put("name", StringUtility.firstLetterLowercase(entityClass.getSimpleName()));
-            entityObj.put("fields", fields);
+            JSONObject entityObj = computeClass(entityClass);
             metadata.put(entityObj);
         }
 
         return metadata;
+    }
+
+    public JSONObject computeClass(Class<?> entityClass) {
+        JSONArray fields = new JSONArray();
+
+        for (Field field : entityClass.getDeclaredFields()) {
+            String type = null;
+
+            Annotation[] annotations = field.getAnnotations();
+
+            for (Class<?> clazz : FIELD_TYPES) {
+                for (Annotation annotation : annotations) {
+                    if (annotation.annotationType().getSimpleName().equals(clazz.getSimpleName())) {
+                        type = clazz.getSimpleName();
+                        break;
+                    }
+
+                    if (type != null) {
+                        break;
+                    }
+                }
+            }
+
+            String className = field.getType().getSimpleName();
+
+            for (Class<?> clazz : GENERICS_TYPES) {
+                if (className.equals(clazz.getSimpleName())) {
+                    String[] items = ((ParameterizedType) field.getGenericType()).getActualTypeArguments()[0].getTypeName().split("\\.");
+                    className = items[items.length - 1];
+                    break;
+                }
+            }
+
+            Boolean nullable = null;
+            Boolean unique = null;
+
+            Column columnAnnotation = field.getAnnotation(Column.class);
+            if (columnAnnotation != null) {
+                nullable = columnAnnotation.nullable();
+                unique = columnAnnotation.unique();
+            }
+
+            JSONObject fieldObj = new JSONObject();
+            fieldObj.put("name", field.getName());
+            fieldObj.put("title", computeTitle(field.getName()));
+            fieldObj.put("type", type);
+            fieldObj.put("class", StringUtility.firstLetterLowercase(className));
+            fieldObj.put("nullable", nullable != null ? nullable : JSONObject.NULL);
+            fieldObj.put("unique", unique != null ? unique : JSONObject.NULL);
+            fields.put(fieldObj);
+        }
+
+        JSONObject entityObj = new JSONObject();
+        entityObj.put("name", StringUtility.firstLetterLowercase(entityClass.getSimpleName()));
+        entityObj.put("title", computeTitle(entityClass.getSimpleName()));
+        entityObj.put("fields", fields);
+        return entityObj;
+    }
+
+    static String computeTitle(String simpleName) {
+        if (simpleName == null || simpleName.equals("")) {
+            return simpleName;
+        }
+
+        String[] items = simpleName.split("(?<=.)(?=\\p{Lu})");
+
+        StringJoiner stringJoiner = new StringJoiner(" ");
+        for (String item : items) {
+            stringJoiner.add(item.substring(0, 1).toUpperCase() + item.substring(1));
+        }
+
+        return stringJoiner.toString();
     }
 }
--- src/test/groovy/org/thehellnet/lanparty/manager/utility/MetadataUtilityTest.groovy
+++ src/test/groovy/org/thehellnet/lanparty/manager/utility/MetadataUtilityTest.groovy
@@ -1,11 +1,14 @@
 package org.thehellnet.lanparty.manager.utility
 
 import org.json.JSONArray
+import org.json.JSONObject
+import org.thehellnet.lanparty.manager.model.persistence.AppUser
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class MetadataUtilityTest extends Specification {
 
-    def "Compute metadata"() {
+    def "Compute all"() {
         given:
         MetadataUtility metadataUtility = MetadataUtility.getInstance()
 
@@ -14,5 +17,40 @@ class MetadataUtilityTest extends Specification {
 
         then:
         println jsonArray.toString()
+    }
+
+    def "Compute single class"() {
+        given:
+        MetadataUtility metadataUtility = MetadataUtility.getInstance()
+
+        when:
+        JSONObject jsonObject = metadataUtility.computeClass(AppUser.class)
+
+        then:
+        println jsonObject.toString()
+    }
+
+    @Unroll
+    def "Compute Class title: #input -> #expected"(String input, String expected) {
+        when:
+        String actual = MetadataUtility.computeTitle(input)
+
+        then:
+        actual == expected
+
+        where:
+        input        | expected
+        null         | null
+        ""           | ""
+        "a"          | "A"
+        "A"          | "A"
+        "Aa"         | "Aa"
+        "AA"         | "A A"
+        "aA"         | "A A"
+        "simple"     | "Simple"
+        "Simple"     | "Simple"
+        "Simplename" | "Simplename"
+        "SimpleName" | "Simple Name"
+        "simpleName" | "Simple Name"
     }
 }
