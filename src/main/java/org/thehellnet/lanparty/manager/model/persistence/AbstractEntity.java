9914f030df345f4730584b7c399c2ba9e4347b1d
sardylan <sardylan@gmail.com>
2020-01-23 23:39:27 +0100

    Adds friendlyName in entities

--- src/main/java/org/thehellnet/lanparty/manager/model/persistence/AbstractEntity.java
+++ src/main/java/org/thehellnet/lanparty/manager/model/persistence/AbstractEntity.java
@@ -1,6 +1,22 @@
 package org.thehellnet.lanparty.manager.model.persistence;
 
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PostLoad;
+import javax.persistence.Transient;
 import java.io.Serializable;
 
+@MappedSuperclass
 public abstract class AbstractEntity implements Serializable {
+
+    @Transient
+    protected String friendlyName;
+
+    public String getFriendlyName() {
+        return friendlyName;
+    }
+
+    @PostLoad
+    private void postLoad() {
+        this.friendlyName = this.toString();
+    }
 }
