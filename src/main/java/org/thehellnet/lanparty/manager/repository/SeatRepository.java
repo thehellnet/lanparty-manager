a44dba48910947c6a071ad747982f9f8fb206080
sardylan <sardylan@gmail.com>
2020-08-02 02:26:30 +0200

    Adds new method in SeatRepository

--- src/main/java/org/thehellnet/lanparty/manager/repository/SeatRepository.java
+++ src/main/java/org/thehellnet/lanparty/manager/repository/SeatRepository.java
@@ -2,10 +2,15 @@ package org.thehellnet.lanparty.manager.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.thehellnet.lanparty.manager.model.persistence.Player;
 import org.thehellnet.lanparty.manager.model.persistence.Seat;
+
+import java.util.List;
 
 @Repository
 public interface SeatRepository extends JpaRepository<Seat, Long> {
 
     Seat findByIpAddress(String ipAddress);
+
+    List<Seat> findAllByPlayer(Player player);
 }
