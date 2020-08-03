3adb1b7ffd8767fe47140207cbbf6110b112d69c
sardylan <sardylan@gmail.com>
2020-08-03 23:54:38 +0200

    Removes unused DTO class

--- src/main/java/org/thehellnet/lanparty/manager/model/dto/response/tool/ToolResponseDTO.java
+++
@@ -1,6 +0,0 @@
-package org.thehellnet.lanparty.manager.model.dto.response.tool;
-
-import org.thehellnet.lanparty.manager.model.dto.response.ResponseDTO;
-
-public abstract class ToolResponseDTO extends ResponseDTO {
-}
