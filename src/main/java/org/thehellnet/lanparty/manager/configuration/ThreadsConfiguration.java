1ab1533572da7bf711d4357d6b015c3b626220dd
sardylan <sardylan@gmail.com>
2020-08-16 16:02:49 +0200

    Restores TaskExecutor

--- src/main/java/org/thehellnet/lanparty/manager/configuration/ThreadsConfiguration.java
+++ src/main/java/org/thehellnet/lanparty/manager/configuration/ThreadsConfiguration.java
@@ -4,16 +4,35 @@ import org.quartz.spi.JobFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.scheduling.quartz.SpringBeanJobFactory;
 import org.thehellnet.lanparty.manager.configuration.mixin.TestAwareConfiguration;
+
+import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
 @EnableScheduling
 public class ThreadsConfiguration implements TestAwareConfiguration {
+
+    @Bean
+    public TaskExecutor getTaskExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setThreadNamePrefix("scheduled-task-");
+        taskExecutor.setDaemon(true);
+        return taskExecutor;
+    }
+
+    @Bean(name = "threadPoolTaskExecutor")
+    public Executor threadPoolTaskExecutor() {
+        return new ThreadPoolTaskExecutor();
+    }
 
     @Bean
     public JobFactory jobFactory(ApplicationContext applicationContext) {
