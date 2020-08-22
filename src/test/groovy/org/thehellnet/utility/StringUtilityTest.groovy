04aef973d3f3efd1fc6edbc73ef60d826fcde0c8
sardylan <sardylan@gmail.com>
2020-08-22 12:27:32 +0200

    Refactor StringUtility tests

--- src/test/groovy/org/thehellnet/utility/StringUtilityTest.groovy
+++ src/test/groovy/org/thehellnet/utility/StringUtilityTest.groovy
@@ -5,150 +5,142 @@ import spock.lang.Unroll
 
 class StringUtilityTest extends Specification {
 
-    def "splitLines"() {
-        given:
-        def DATA = [
-                ""                  : [],
-                " "                 : [],
-                "  "                : [],
-                "\n"                : [],
-                " \n"               : [],
-                "\n "               : [],
-                " \n "              : [],
-                "\n\n"              : [],
-                "\n \n"             : [],
-                " \n \n"            : [],
-                " \n \n "           : [],
-                " \n  \n "          : [],
-                "\n\r"              : [],
-                "\n \r"             : [],
-                " \n \r"            : [],
-                " \n \r "           : [],
-                " \n  \r "          : [],
-                "test"              : ["test"],
-                " test"             : ["test"],
-                "test "             : ["test"],
-                " test "            : ["test"],
-                "test test"         : ["test test"],
-                "test test "        : ["test test"],
-                " test test"        : ["test test"],
-                " test test "       : ["test test"],
-                " test    test "    : ["test    test"],
-                " test   \n test "  : ["test", "test"],
-                " test   \n test\n ": ["test", "test"],
-                "test\nTEST\ntest"  : ["test", "TEST", "test"],
-                " test   \n test\r ": ["test", "test"],
-                "test\nTEST\rtest"  : ["test", "TEST", "test"]
-        ]
+    @Unroll
+    def "splitLines \"#input\" \"#expected\""(String input, List<String> expected) {
+        when:
+        List<String> actual = StringUtility.splitLines(input)
 
-        expect:
-        assert StringUtility.splitLines(null) == null
+        then:
+        actual == expected
 
-        DATA.each {
-            if (StringUtility.splitLines(it.key) != it.value) {
-                assert false
-            }
-        }
-    }
-
-    def "joinLines"() {
-        given:
-        def DATA = [
-                []                      : "",
-                ["test"]                : "test",
-                ["test test"]           : "test test",
-                ["test", "test"]        : "test\ntest",
-                ["test", " test"]       : "test\ntest",
-                ["test ", "test"]       : "test\ntest",
-                ["test ", " test"]      : "test\ntest",
-                ["test ", " test "]     : "test\ntest",
-                [" test ", " test "]    : "test\ntest",
-                ["test", "TEST", "test"]: "test\nTEST\ntest"
-        ]
-
-        expect:
-        assert StringUtility.joinLines(null) == null
-
-        DATA.each {
-            if (StringUtility.joinLines(it.key) != it.value) {
-                assert false
-            }
-        }
-    }
-
-    def "splitSpaces"() {
-        given:
-        def DATA = [
-                ""                  : [],
-                " "                 : [],
-                "  "                : [],
-                "\n"                : [],
-                " \n"               : [],
-                "\n "               : [],
-                " \n "              : [],
-                "\n\n"              : [],
-                "\n \n"             : [],
-                " \n \n"            : [],
-                " \n \n "           : [],
-                " \n  \n "          : [],
-                "\n\r"              : [],
-                "\n \r"             : [],
-                " \n \r"            : [],
-                " \n \r "           : [],
-                " \n  \r "          : [],
-                "test"              : ["test"],
-                " test"             : ["test"],
-                "test "             : ["test"],
-                " test "            : ["test"],
-                "test test"         : ["test", "test"],
-                "test test "        : ["test", "test"],
-                " test test"        : ["test", "test"],
-                " test test "       : ["test", "test"],
-                " test    test "    : ["test", "test"],
-                " test   \n test "  : ["test", "test"],
-                " test   \n test\n ": ["test", "test"],
-                "test\nTEST\ntest"  : ["testTESTtest"],
-                " test   \n test\r ": ["test", "test"],
-                "test\nTEST\rtest"  : ["testTESTtest"]
-        ]
-
-        expect:
-        assert StringUtility.splitSpaces(null) == null
-
-        DATA.each {
-            if (StringUtility.splitSpaces(it.key) != it.value) {
-                assert false
-            }
-        }
-    }
-
-    def "joinSpaces"() {
-        given:
-        def DATA = [
-                []                      : "",
-                ["test"]                : "test",
-                ["test test"]           : "test test",
-                ["test", "test"]        : "test test",
-                ["test", " test"]       : "test test",
-                ["test ", "test"]       : "test test",
-                ["test ", " test"]      : "test test",
-                ["test ", " test "]     : "test test",
-                [" test ", " test "]    : "test test",
-                ["test", "TEST", "test"]: "test TEST test"
-        ]
-
-        expect:
-        assert StringUtility.joinSpaces(null) == null
-
-        DATA.each {
-            if (StringUtility.joinSpaces(it.key) != it.value) {
-                assert false
-            }
-        }
+        where:
+        input                | expected
+        null                 | []
+        ""                   | []
+        " "                  | []
+        "  "                 | []
+        "\n"                 | []
+        " \n"                | []
+        "\n "                | []
+        " \n "               | []
+        "\n\n"               | []
+        "\n \n"              | []
+        " \n \n"             | []
+        " \n \n "            | []
+        " \n  \n "           | []
+        "\n\r"               | []
+        "\n \r"              | []
+        " \n \r"             | []
+        " \n \r "            | []
+        " \n  \r "           | []
+        "test"               | ["test"]
+        " test"              | ["test"]
+        "test "              | ["test"]
+        " test "             | ["test"]
+        "test test"          | ["test test"]
+        "test test "         | ["test test"]
+        " test test"         | ["test test"]
+        " test test "        | ["test test"]
+        " test    test "     | ["test    test"]
+        " test   \n test "   | ["test", "test"]
+        " test   \n test\n " | ["test", "test"]
+        "test\nTEST\ntest"   | ["test", "TEST", "test"]
+        " test   \n test\r " | ["test", "test"]
+        "test\nTEST\rtest"   | ["test", "TEST", "test"]
     }
 
     @Unroll
-    def "firstLetterLowercase #input #expected"(String input, String expected) {
+    def "joinLines \"#input\" \"#expected\""(List<String> input, String expected) {
+        when:
+        String actual = StringUtility.joinLines(input)
+
+        then:
+        actual == expected
+
+        where:
+        input                    | expected
+        null                     | ""
+        []                       | ""
+        ["test"]                 | "test"
+        ["test test"]            | "test test"
+        ["test", "test"]         | "test\ntest"
+        ["test", " test"]        | "test\ntest"
+        ["test ", "test"]        | "test\ntest"
+        ["test ", " test"]       | "test\ntest"
+        ["test ", " test "]      | "test\ntest"
+        [" test ", " test "]     | "test\ntest"
+        ["test", "TEST", "test"] | "test\nTEST\ntest"
+    }
+
+    @Unroll
+    def "splitSpaces \"#input\" \"#expected\""(String input, List<String> expected) {
+        when:
+        List<String> actual = StringUtility.splitSpaces(input)
+
+        then:
+        actual == expected
+
+        where:
+        input                | expected
+        null                 | null
+        ""                   | []
+        " "                  | []
+        "  "                 | []
+        "\n"                 | []
+        " \n"                | []
+        "\n "                | []
+        " \n "               | []
+        "\n\n"               | []
+        "\n \n"              | []
+        " \n \n"             | []
+        " \n \n "            | []
+        " \n  \n "           | []
+        "\n\r"               | []
+        "\n \r"              | []
+        " \n \r"             | []
+        " \n \r "            | []
+        " \n  \r "           | []
+        "test"               | ["test"]
+        " test"              | ["test"]
+        "test "              | ["test"]
+        " test "             | ["test"]
+        "test test"          | ["test", "test"]
+        "test test "         | ["test", "test"]
+        " test test"         | ["test", "test"]
+        " test test "        | ["test", "test"]
+        " test    test "     | ["test", "test"]
+        " test   \n test "   | ["test", "test"]
+        " test   \n test\n " | ["test", "test"]
+        "test\nTEST\ntest"   | ["testTESTtest"]
+        " test   \n test\r " | ["test", "test"]
+        "test\nTEST\rtest"   | ["testTESTtest"]
+    }
+
+    @Unroll
+    def "joinSpaces \"#input\" \"#expected\""(List<String> input, String expected) {
+        when:
+        String actual = StringUtility.joinSpaces(input)
+
+        then:
+        actual == expected
+
+        where:
+        input                    | expected
+        null                     | ""
+        []                       | ""
+        ["test"]                 | "test"
+        ["test test"]            | "test test"
+        ["test", "test"]         | "test test"
+        ["test", " test"]        | "test test"
+        ["test ", "test"]        | "test test"
+        ["test ", " test"]       | "test test"
+        ["test ", " test "]      | "test test"
+        [" test ", " test "]     | "test test"
+        ["test", "TEST", "test"] | "test TEST test"
+    }
+
+    @Unroll
+    def "firstLetterLowercase \"#input\" \"#expected\""(String input, String expected) {
         when:
         String actual = StringUtility.firstLetterLowercase(input)
 
